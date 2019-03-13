8fe6262d6bba8520dfdf38db5a98e18903a83cf1
Thomas Backmark <total@viskos.org>
2019-03-13 21:35:44 +0100

    Added Blue brinstar and logic to East Crateria

---
+++ Randomizer.SuperMetroid/Regions/Brinstar/Blue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using static Randomizer.SuperMetroid.ItemType;
+using static Randomizer.SuperMetroid.Difficulty;
+
+namespace Randomizer.SuperMetroid.Regions.Brinstar {
+
+    class Blue : Region {
+
+        public override string Name => "Blue Brinstar";
+        public override string Area => "Brinstar";
+
+        public Blue(World world, Difficulty difficulty) : base(world, difficulty) {
+            Locations = new List<Location> {
+                new Location(this, "Morphing Ball", LocationType.Visible, 0x786EC),
+                new Location(this, "Power Bomb (blue Brinstar)", LocationType.Visible, 0x7874C, Difficulty switch {
+                    _ => new Requirement(items => items.CanUsePowerBombs())
+                }),
+                new Location(this, "Missile (blue Brinstar middle)", LocationType.Visible, 0x78798, Difficulty switch {
+                    _ => new Requirement(items => items.Has(Morph))
+                }),
+                new Location(this, "Energy Tank, Brinstar Ceiling", LocationType.Hidden, 0x7879E, Difficulty switch {
+                    Casual => items => items.CanFly() || items.Has(HiJump) ||items.Has(SpeedBooster) || items.Has(IceBeam),
+                    _ => new Requirement(items => true)
+                }),
+                new Location(this, "Missile (blue Brinstar bottom)", LocationType.Chozo, 0x78802, Difficulty switch {
+                    _ => new Requirement(items => items.Has(Morph))
+                }),
+                new Location(this, "Missile (blue Brinstar top)", LocationType.Visible, 0x78836, Difficulty switch {
+                    _ => new Requirement(items => items.CanUsePowerBombs())
+                }),
+                new Location(this, "Missile (blue Brinstar behind missile)", LocationType.Hidden, 0x7883C, Difficulty switch {
+                    _ => new Requirement(items => items.CanUsePowerBombs())
+                }),
+            };
+        }
+    }
+}
--- Randomizer.SuperMetroid/Regions/Crateria/East.cs
+++ Randomizer.SuperMetroid/Regions/Crateria/East.cs
@@ -11,14 +11,27 @@ namespace Randomizer.SuperMetroid.Regions.Crateria {
 
         public East(World world, Difficulty difficulty) : base(world, difficulty) {
             Locations = new List<Location> {
-
+                new Location(this, "Missile (outside Wrecked Ship bottom)", LocationType.Visible, 0x781E8, Difficulty switch {
+                    Casual => items => items.Has(SpaceJump) || items.Has(SpeedBooster) || items.Has(Grapple),
+                    _ => new Requirement(items => true)
+                }),
+                new Location(this, "Missile (outside Wrecked Ship top)", LocationType.Hidden, 0x781EE, Difficulty switch {
+                    Casual => items => items.Has(Super) && items.CanPassBombPassages() && (items.Has(SpaceJump) || items.Has(SpeedBooster) || items.Has(Grapple)),
+                    _ => new Requirement(items => items.Has(Super) && items.CanPassBombPassages())
+                }),
+                new Location(this, "Missile (outside Wrecked Ship middle)", LocationType.Visible, 0x781F4, Difficulty switch {
+                    Casual => items => items.Has(Super) && items.CanPassBombPassages() && (items.Has(SpaceJump) || items.Has(SpeedBooster) || items.Has(Grapple)),
+                    _ => new Requirement(items => items.Has(Super) && items.CanPassBombPassages())
+                }),
+                new Location(this, "Missile (Crateria moat)", LocationType.Visible, 0x78248, Difficulty switch {
+                    Casual => items => items.Has(SpaceJump) || items.Has(SpeedBooster) || items.Has(Grapple),
+                    _ => new Requirement(items => true)
+                }),
             };
         }
 
         public override bool CanEnter(List<Item> items) {
-            return base.CanEnter(items);
+            return items.CanUsePowerBombs() && items.Has(Super);
         }
-
     }
-
 }
