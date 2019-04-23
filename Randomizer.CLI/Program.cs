2171a8a57ede0928af027cb9f74e0c42ed045525
Rebelus Quo <rebelusquo@tutanota.com>
2019-04-23 23:48:34 +0200

    Produce patch in json format from CLI
    It also adds a --no-interact option to make it easier to redirect output to a file.

--- Randomizer.CLI/Program.cs
+++ Randomizer.CLI/Program.cs
@@ -31,15 +31,25 @@ namespace Randomizer.CLI {
                 HelpText = "Generate a specific seed")]
             public string Seed { get; set; } = string.Empty;
 
+            [Option("no-interact",
+                HelpText = "Do not wait for keyboard input")]
+            public bool NoInteract { get; set; }
+
             [Option("playthrough",
                 HelpText = "Show json formated playthrough for each seed")]
             public bool Playthrough { get; set; }
+
+            [Option("patch",
+                HelpText = "Show json formated patch for each world in the seed")]
+            public bool Patch { get; set; }
 
             public string Logic => this switch {
                 var o when o.Tournament => "tournament",
                 var o when o.Casual => "casual",
                 _ => "tournament"
             };
+
+            public bool Interact => !NoInteract;
 
             public abstract IRandomizer NewRandomizer();
 
@@ -92,9 +102,44 @@ namespace Randomizer.CLI {
                 $"Generation time: {DateTime.Now - time}"
             ));
             if (opts.Playthrough) {
-                Console.WriteLine(JsonConvert.SerializeObject(data.Playthrough));
-                Console.ReadLine();
+                Console.WriteLine(JsonConvert.SerializeObject(data.Playthrough, Formatting.Indented));
+                Interact(opts);
             }
+            if (opts.Patch) {
+                Console.WriteLine(JsonConvert.SerializeObject(
+                    data.Worlds.ToDictionary(x => x.Player, x => x.Patches), Formatting.Indented,
+                    new PatchWriteConverter()
+                ));
+                Interact(opts);
+            }
+        }
+
+        static void Interact(CliOptions opts) {
+            if (opts.Interact) Console.ReadLine();
+        }
+
+        public class PatchWriteConverter : JsonConverter<IDictionary<int, byte[]>> {
+
+            public override void WriteJson(JsonWriter writer, IDictionary<int, byte[]> value, JsonSerializer serializer) {
+                writer.WriteStartObject();
+                foreach (var (address, bytes) in value) {
+                    var width = address > 0x00FFFFFF ? 8 : address > 0x0000FFFF ? 6 : 4;
+                    writer.WritePropertyName(address.ToString($"X{width}"));
+                    writer.WriteValue(string.Join("", bytes.Select(b => b.ToString("X2"))));
+                }
+                writer.WriteEndObject();
+            }
+
+            #region Can not read
+
+            public override bool CanRead => false;
+
+            public override IDictionary<int, byte[]> ReadJson(JsonReader reader, Type objectType, IDictionary<int, byte[]> existingValue, bool hasExistingValue, JsonSerializer serializer) {
+                throw new NotImplementedException("This converter can not read data");
+            }
+
+            #endregion
+
         }
 
     }
