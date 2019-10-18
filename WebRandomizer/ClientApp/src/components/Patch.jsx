117b0fdb60788ec912a76f29620f986347d09bdf
total <total@ceres.viskos.org>
2019-10-18 12:49:20 +0200

    Fixed duplication detection bug. Made the base patch non-cacheable.

--- WebRandomizer/ClientApp/src/components/Patch.jsx
+++ WebRandomizer/ClientApp/src/components/Patch.jsx
@@ -117,7 +117,7 @@ export class Patch extends Component {
     async prepareRom(world_patch) {
         const rom_blob = await this.localForage.getItem("baseRomCombo");
         const rom = new Uint8Array(await readAsArrayBuffer(rom_blob));
-        const base_patch = this.checkDecompress(new Uint8Array(await (await fetch(baseIps)).arrayBuffer()));
+        const base_patch = this.checkDecompress(new Uint8Array(await (await fetch(baseIps, { cache: "no-store" })).arrayBuffer()));
         world_patch = Uint8Array.from(atob(world_patch), c => c.charCodeAt(0));
 
         applyIps(rom, base_patch);
