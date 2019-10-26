d36118922f080a0a86f66cef61e8cd21a2edcdba
total <total@ceres.viskos.org>
2019-10-26 21:35:46 +0200

    Add support for Markdown documents

--- WebRandomizer/ClientApp/src/App.js
+++ WebRandomizer/ClientApp/src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
+import { Markdown } from './components/Markdown';
 import { Randomizer } from './components/Randomizer';
 import { Multiworld } from './components/Multiworld';
+
+import instructionsMd from './files/markdown/instructions.md';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -13,6 +16,7 @@ export default class App extends Component {
       <Layout>
             <Route exact path='/' component={Home} />
             <Route path='/randomizer' component={Randomizer} />
+            <Route path='/instructions' render={(props) => <Markdown {...props} mdLink={instructionsMd} />} />
             <Route path='/multiworld/:session_id' component={Multiworld} />
       </Layout>
     );
---
+++ WebRandomizer/ClientApp/src/files/markdown/instructions.md
@@ -0,0 +1,20 @@
+# Multiworld Beta Instructions
+
+This page will help you get setup and ready to play SMZ3 Multiworld.
+
+## Thing 1
+
+This thing has a link [Super Happy Link](https://samus.link/)
+
+## Thing 2
+
+Hello table
+
+| Great      | Best |
+| ---------- | ---- |
+| table      | yay  |
+
+-------------------------------
+
+## Thing 3
+
