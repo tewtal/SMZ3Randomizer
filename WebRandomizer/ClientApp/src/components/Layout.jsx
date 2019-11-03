5bd17db33fc7ed37d8f8eaf7efc72e70b7cbdc0b
Rebelus Quo <rebelusquo@tutanota.com>
2019-11-03 18:20:40 +0100

    Turn NavMenu css into global styled component
    Since we already have styled-components available to us we can skip importing css files in favor of grouping the css style with the component it affects. The general html selectors in NavMenu.css goes with the Layout, since it affects the page as a whole, while the other selectors relate to the NavMenu component.
    
    Note that style-components can nest selectors just like in css processors.

--- WebRandomizer/ClientApp/src/components/Layout.jsx
+++ WebRandomizer/ClientApp/src/components/Layout.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 import { Container } from 'reactstrap';
 import NavMenu from './NavMenu';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 14px;
+    @media(min-width: 768px) {
+      font-size: 16px;
+    }
+  }
+`;
 
 export default function Layout(props) {
     return (
         <>
+            <GlobalStyle />
             <NavMenu />
             <Container>
                 {props.children}
