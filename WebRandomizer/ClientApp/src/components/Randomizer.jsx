b926c23e7d61c5f6b27c2e5588d09c0a452a4119
Rebelus Quo <rebelusquo@tutanota.com>
2019-11-05 04:47:10 +0100

    Convert Randomizer into a react function
    Since react already uses an `on` prefix for event handlers we'll use that to signal that a function is intended as an event handler.
    
    Also, remove password state and just default to sending an empty string.

--- WebRandomizer/ClientApp/src/components/Randomizer.jsx
+++ WebRandomizer/ClientApp/src/components/Randomizer.jsx
@@ -1,164 +1,135 @@
-﻿import React, { Component } from 'react';
-import { Container, Row, Col, Card, CardHeader, CardBody, Button, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Modal, ModalHeader, ModalBody, Progress } from 'reactstrap';
+﻿import React, { useState } from 'react';
+import { Container, Row, Col, Card, CardHeader, CardBody } from 'reactstrap';
+import { Form, Button, Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, Progress } from 'reactstrap';
 
-export default class Randomizer extends Component {
-    static displayName = Randomizer.name;
-    constructor(props) {
-        super(props);
-        this.state = { showGeneratingDialog: false, seed: [], players: 2, playerNames: ["", ""], logic: "casual", seedNumber: "", password: "" };
-        this.updatePlayers = this.updatePlayers.bind(this);
-        this.updatePlayerName = this.updatePlayerName.bind(this);
-        this.updateLogic = this.updateLogic.bind(this);
-        this.updateSeedNumber = this.updateSeedNumber.bind(this);
-        this.updatePassword = this.updatePassword.bind(this);
-        this.createGame = this.createGame.bind(this);
-    }
+import times from 'lodash/times';
+import fromPairs from 'lodash/fromPairs';
 
-    async createGame(e) {
-        e.preventDefault();
+export default function Randomizer(props) {
+    const [seed, setSeed] = useState('');
+    const [logic, setLogic] = useState('casual');
+    const [worlds, setWorlds] = useState(2);
+    const [players, setPlayers] = useState(times(2, () => ''));
+    const [generating, setGenerating] = useState(false);
 
-        this.setState({ showGeneratingDialog: true });
+    async function createGame() {
+        setGenerating(true);
 
         try {
-            let opts = { "mode": "multiworld", "logic": this.state.logic, "worlds": this.state.players, "seed": this.state.seedNumber, "password": this.state.password };
+            const options = {
+                mode: 'multiworld',
+                password: '',
+                seed,
+                logic,
+                worlds,
+                ...fromPairs(times(worlds, i => [`player-${i}`, players[i]])),
+            };
 
-            for (let p = 0; p < this.state.players; p++) {
-                opts["player-" + p] = this.state.playerNames[p];
-            }
-
-            let response = await fetch("/api/randomizer/generate",
-                {
-                    method: "POST",
-                    cache: "no-cache",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({ options: opts })
-                });
-            let data = await response.json();
-            this.setState({ showGeneratingDialog: false });
-            this.props.history.push('/multiworld/' + data.guid);
+            const response = await fetch('/api/randomizer/generate', {
+                method: 'POST',
+                cache: 'no-cache',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ options })
+            });
+            const data = await response.json();
+            setGenerating(false);
+            props.history.push(`/multiworld/${data.guid}`);
         } catch (error) {
-            this.setState({ showGeneratingDialog: false });
+            setGenerating(false);
             console.log("Error:", error);
         }
     }
 
-    updatePlayers(e) {
-        let players = e.target.value;
-        let playerNames = this.state.playerNames;
-
-        for (let p = 0; p < this.state.players; p++) {
-            if (playerNames[p] === undefined) {
-                playerNames[p] = ""
-            }
-        }
-
-        this.setState({ playerNames: playerNames, players: players });
+    function onPlayerCountChange(e) {
+        const worlds = e.target.value;
+        /* default any additional player name */
+        times(worlds, i => players[i] = players[i] || '')
+        setWorlds(worlds);
+        setPlayers(players);
     }
 
-    updatePlayerName(e) {
-        let playerId = e.target.id.split('-')[1];
-        let playerNames = this.state.playerNames;
-
-        playerNames[playerId] = e.target.value;
-
-        this.setState({
-            playerNames: playerNames
-        });
+    function onPlayerChange(e, index) {
+        setPlayers(players.map((name, i) => i !== index ? name : e.target.value));
     }
 
-    updateLogic(e) {
-        this.setState({ logic: e.target.value });
-    }
-
-    updateSeedNumber(e) {
-        this.setState({ seedNumber: e.target.value });
-    }
-
-    updatePassword(e) {
-        this.setState({ password: e.target.value });
-    }
-
-    render() {
-
-        let playerInputs = [];
-
-        for (let p = 0; p < this.state.players; p++) {
-            playerInputs.push(
-                <Col key={"playerInput" + p} md={{ span: 5, offset: (1 - (p % 2)) }}>
+    const form = (
+        <Form onSubmit={(e) => { e.preventDefault(); createGame(); }}>
+            <Row className="form-group">
+                <Col md="3">
                     <InputGroup>
                         <InputGroupAddon addonType="prepend">
-                            <InputGroupText>Name {p + 1}</InputGroupText>
+                            <InputGroupText>Players</InputGroupText>
                         </InputGroupAddon>
-                        <Input id={"player-" + p} defaultValue={this.state.playerNames[p]} onChange={this.updatePlayerName} />
+                        <Input value={worlds} onChange={onPlayerCountChange} />
                     </InputGroup>
-                </Col>);
-        }
-
-        return (
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Col md="8">
-                        <Card>
-                            <CardHeader className="bg-primary text-white">
-                                New Randomized Game - SMZ3 Multiworld 1.0-beta2
-                            </CardHeader>
-                            <CardBody>
-                                <Form onSubmit={this.createGame}>
-                                    <Row className="form-group">
-                                        <Col md="3">
-                                            <InputGroup>
-                                                <InputGroupAddon addonType="prepend">
-                                                    <InputGroupText>Players</InputGroupText>
-                                                </InputGroupAddon>
-                                                <Input id="players" defaultValue={this.state.players} onChange={this.updatePlayers} />
-                                            </InputGroup>
-                                        </Col>
-                                        <Col md="3">
-                                            <InputGroup>
-                                                <InputGroupAddon addonType="prepend">
-                                                    <InputGroupText>Seed</InputGroupText>
-                                                </InputGroupAddon>
-                                                <Input id="seed" defaultValue={this.state.seedNumber} onChange={this.updateSeedNumber} />
-                                            </InputGroup>
-                                        </Col>
-                                        <Col md="1"></Col>
-                                        <Col md="5">
-                                            <InputGroup>
-                                                <InputGroupAddon addonType="prepend">
-                                                    <InputGroupText>Logic</InputGroupText>
-                                                </InputGroupAddon>
-                                                <Input type="select" id="logic" defaultValue={this.state.logic} onChange={this.updateLogic}>
-                                                    <option value="casual">Casual</option>
-                                                    <option value="tournament">Tournament</option>
-                                                </Input>
-                                            </InputGroup>
-                                        </Col>
-                                    </Row>
-                                    <Row className="form-group">
-                                        {playerInputs}
-                                    </Row>
-                                    <Row className="form-group">
-                                        <Col md="6">
-                                            <Button color="success" type="submit">Create Multiworld Game</Button>
-                                        </Col>
-                                    </Row>
-                                </Form>
-                            </CardBody>
-                        </Card>
+                </Col>
+                <Col md="3">
+                    <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                            <InputGroupText>Seed</InputGroupText>
+                        </InputGroupAddon>
+                        <Input value={seed} onChange={(e) => setSeed(e.target.value)} />
+                    </InputGroup>
+                </Col>
+                <Col md="1" />
+                <Col md="5">
+                    <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                            <InputGroupText>Logic</InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="select" value={logic} onChange={(e) => setLogic(e.target.value)}>
+                            <option value="casual">Casual</option>
+                            <option value="tournament">Tournament</option>
+                        </Input>
+                    </InputGroup>
+                </Col>
+            </Row>
+            <Row className="form-group">
+                {players.slice(0, worlds).map((name, i) =>
+                    <Col key={`player${i}`} md={{ span: 5, offset: (1 - (i % 2)) }}>
+                        <InputGroup>
+                            <InputGroupAddon addonType="prepend">
+                                <InputGroupText>Name {i + 1}</InputGroupText>
+                            </InputGroupAddon>
+                            <Input value={name} onChange={(e) => onPlayerChange(e, i)} />
+                        </InputGroup>
                     </Col>
-                </Row>
+                )}
+            </Row>
+            <Row className="form-group">
+                <Col md="6">
+                    <Button type="submit" color="success">Create Multiworld Game</Button>
+                </Col>
+            </Row>
+        </Form>
+    );
 
-                <Modal isOpen={this.state.showGeneratingDialog} backdrop="static" autoFocus>
-                    <ModalHeader>Generating new game</ModalHeader>
-                    <ModalBody>
-                        <p>Please wait while a new game is generated</p>
-                        <Progress animated color="info" value={100} />
-                    </ModalBody>
-                </Modal>
+    const modal = (
+        <Modal isOpen={generating} backdrop="static" autoFocus>
+            <ModalHeader>Generating new game</ModalHeader>
+            <ModalBody>
+                <p>Please wait while a new game is generated</p>
+                <Progress animated color="info" value={100} />
+            </ModalBody>
+        </Modal>
+    );
 
-            </Container>
-        );
-    }
+    return (
+        <Container>
+            <Row className="justify-content-md-center">
+                <Col md="8">
+                    <Card>
+                        <CardHeader className="bg-primary text-white">
+                            New Randomized Game - SMZ3 Multiworld 1.0-beta2
+                        </CardHeader>
+                        <CardBody>
+                            {form}
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+            {modal}
+        </Container>
+    );
 }
