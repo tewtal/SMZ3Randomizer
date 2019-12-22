a74f7349e6162da181a1fe3cbdcb68bfc00c7097
Thomas Backmark <total@viskos.org>
2019-12-22 18:24:26 +0100

    Rewrote Configure to Hooks-style

--- WebRandomizer/ClientApp/src/components/Configure.js
+++ WebRandomizer/ClientApp/src/components/Configure.js
@@ -1,63 +1,61 @@
-﻿import React, { Component } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Card, CardHeader, CardBody, Button, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Modal, ModalHeader, ModalBody, Progress } from 'reactstrap';
 
-export class Configure extends Component {
-    static displayName = Configure.name;
-    constructor(props) {
-        super(props);
-        this.state = { showGeneratingDialog: false, options: [], randomizer: null, error: false };
-        this.randomizer_id = this.props.match.params.randomizer_id;
-        this.options = [];
-    }
+export function Configure(props) {
+    const [options, setOptions] = useState(null);
+    const [modal, setModal] = useState(false);
+    const [randomizer, setRandomizer] = useState(null);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const randomizer_id = props.match.params.randomizer_id;
 
-    componentDidMount() {
-        fetch(`/api/randomizers/${this.randomizer_id}`)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    if (result) {
-                        this.options = result.options.reduce((obj, opt) => { return { ...obj, [opt.key]: opt.default !== null ? opt.default : "" }; }, {});
-                        this.setState({ randomizer: result, options: this.options });
-                    } else {
-                        this.setState({
-                            error: true,
-                            errorMessage: `Could not load randomizer metadata for: ${this.randomizer_id}`
-                        });
-                    }
-                },
-                (error) => this.setState({ error: true, errorMessage: error })
-            );
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                var response = await fetch(`/api/randomizers/${randomizer_id}`);
+                if (response) {
+                    var result = await response.json();
+                    setOptions(result.options.reduce((obj, opt) => { return { ...obj, [opt.key]: opt.default !== null ? opt.default : "" }; }, {}));
+                    setRandomizer(result);
+                } else {
+                    setErrorMessage("Cannot load metadata for the specified randomizer.");
+                }
+            } catch (error) {
+                setErrorMessage(error);
+            }
+        };
 
-    createGame = async (e) => {
+        fetchData();
+    }, []);
+
+    async function createGame(e) {
         e.preventDefault();
-        this.setState({ showGeneratingDialog: true });
+        setModal(true);
 
-        // temp fix
-        this.options["mode"] = "multiworld";
-        this.options["worlds"] = "1";
-        this.options["player-0"] = "Player 1";
+        // temp fix        
+        options["mode"] = "multiworld";
+        options["worlds"] = "1";
+        options["player-0"] = "Player 1";
 
-        let response = await fetch(`/api/randomizers/${this.randomizer_id}/generate`,
+        let response = await fetch(`/api/randomizers/${randomizer_id}/generate`,
             {
                 method: "POST",
                 cache: "no-cache",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ options: this.options })
+                body: JSON.stringify({ options: options })
             });
         let data = await response.json();
+        // TODO: Temp, write spoiler to console log
         console.log(data.spoiler);
-        this.setState({ showGeneratingDialog: false });
+        setModal(false);
     }
 
-    updateOption = (e, key) => {
-        this.options[key] = e.target.value;
-        this.setState({ options: this.options });
+    function updateOption(e, key) {
+        setOptions({...options, [key]: e.target.value});
     }
 
-    chunk(array, size) {
+    function chunk(array, size) {
         return array.reduce((chunks, item, i) => {
             if (i % size === 0) {
                 chunks.push([item]);
@@ -68,97 +66,95 @@ export class Configure extends Component {
         }, []);
     }
 
-    render() {
-        if (this.state.randomizer) {
-            const options = this.state.randomizer.options.map(opt => {
-                let inputElement = null;
-                switch (opt.type) {
-                    case 'input':
-                        inputElement = (
-                            <Col key={opt.key} md="4">
-                                <InputGroup>
-                                    <InputGroupAddon addonType="prepend">
-                                        <InputGroupText>{opt.description}</InputGroupText>
-                                    </InputGroupAddon>
-                                    <Input id={opt.key} defaultValue={this.state.options[opt.key]} onChange={(e) => this.updateOption(e, opt.key)} />
-                                </InputGroup>
-                            </Col>
-                        );
-                        break;
-                    case 'dropdown':
-                        inputElement = (
-                            <Col key={opt.key} md="4">
-                                <InputGroup>
-                                    <InputGroupAddon addonType="prepend">
-                                        <InputGroupText>{opt.description}</InputGroupText>
-                                    </InputGroupAddon>
-                                    <Input type="select" id={opt.key} defaultValue={this.state.options[opt.key]} onChange={(e) => this.updateOption(e, opt.key)}>
-                                        {Object.entries(opt.values).map(([k,v]) => <option key={k} value={k}>{v}</option>)}
-                                    </Input>
-                                </InputGroup>
-                            </Col>
-                        );
-                        break;
-                }
+    if (randomizer) {
+        const formOptions = randomizer.options.map(opt => {
+            let inputElement = null;
+            switch (opt.type) {
+                case 'input':
+                    inputElement = (
+                        <Col key={opt.key} md="4">
+                            <InputGroup>
+                                <InputGroupAddon addonType="prepend">
+                                    <InputGroupText>{opt.description}</InputGroupText>
+                                </InputGroupAddon>
+                                <Input id={opt.key} defaultValue={options[opt.key]} onChange={(e) => updateOption(e, opt.key)} />
+                            </InputGroup>
+                        </Col>
+                    );
+                    break;
+                case 'dropdown':
+                    inputElement = (
+                        <Col key={opt.key} md="4">
+                            <InputGroup>
+                                <InputGroupAddon addonType="prepend">
+                                    <InputGroupText>{opt.description}</InputGroupText>
+                                </InputGroupAddon>
+                                <Input type="select" id={opt.key} defaultValue={options[opt.key]} onChange={(e) => updateOption(e, opt.key)}>
+                                    {Object.entries(opt.values).map(([k,v]) => <option key={k} value={k}>{v}</option>)}
+                                </Input>
+                            </InputGroup>
+                        </Col>
+                    );
+                    break;
+            }
 
-                return inputElement;
-            });
+            return inputElement;
+        });
 
-            const optionGroups = this.chunk(options, 3);
+        const formOptionGroups = chunk(formOptions, 3);
 
-            return (
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col md="10">
-                            <Card>
-                                <CardHeader className="bg-primary text-white">
-                                    {this.state.randomizer.name} - {this.state.randomizer.version}
-                                </CardHeader>
-                                <CardBody>
-                                    <Form onSubmit={this.createGame}>
-                                        {optionGroups.map((options, i) => (
-                                            <Row key={i} className="form-group">
-                                                {options}
-                                            </Row>
-                                        ))}
-                                        <Row className="form-group">
-                                            <Col md="6">
-                                                <Button color="success" type="submit">Generate Game</Button>
-                                            </Col>
+        return (
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col md="10">
+                        <Card>
+                            <CardHeader className="bg-primary text-white">
+                                {randomizer.name} - {randomizer.version}
+                            </CardHeader>
+                            <CardBody>
+                                <Form onSubmit={createGame}>
+                                    {formOptionGroups.map((optionGroup, i) => (
+                                        <Row key={i} className="form-group">
+                                            {optionGroup}
                                         </Row>
-                                    </Form>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
+                                    ))}
+                                    <Row className="form-group">
+                                        <Col md="6">
+                                            <Button color="success" type="submit">Generate Game</Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
 
-                    <Modal isOpen={this.state.showGeneratingDialog} backdrop="static" autoFocus>
-                        <ModalHeader>Generating new game</ModalHeader>
-                        <ModalBody>
-                            <p>Please wait while a new game is generated</p>
-                            <Progress animated color="info" value={100} />
-                        </ModalBody>
-                    </Modal>
+                <Modal isOpen={modal} backdrop="static" autoFocus>
+                    <ModalHeader>Generating new game</ModalHeader>
+                    <ModalBody>
+                        <p>Please wait while a new game is generated</p>
+                        <Progress animated color="info" value={100} />
+                    </ModalBody>
+                </Modal>
 
-                </Container>
-            );
-        } else {
-            return (
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col md="10">
-                            <Card>
-                                <CardHeader className={this.state.error === false ? "bg-primary text-white" : "bg-danger text-white"}>
-                                    {this.state.error === false ? <div>Create new randomized game</div> : <div>Something went wrong :(</div>}
-                                </CardHeader>
-                                <CardBody>
-                                    {this.state.error === false ? <p>Please wait, loading...</p> : <p>{this.state.errorMessage}</p>}
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            );
-        }
+            </Container>
+        );
+    } else {
+        return (
+            <Container>
+                <Row className="justify-content-md-center">
+                    <Col md="10">
+                        <Card>
+                            <CardHeader className={errorMessage ? "bg-danger text-white" : "bg-primary text-white"}>
+                                {errorMessage ? <div>Something went wrong :(</div> : <div>Create new randomized game</div>}
+                            </CardHeader>
+                            <CardBody>
+                                {errorMessage ? <p>{errorMessage}</p> : <p>Please wait, loading...</p>}
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        );
     }
 }
