4c6a4f23991cb10c434454463a3bf1ed7b19b147
TarThoron <62519520+TarThoron@users.noreply.github.com>
2020-03-26 17:34:50 -0500

    Fix access from Maridia Portal
    Also fixes missing red door requirement on mama turtle items.

--- Randomizer.SMZ3/Regions/SuperMetroid/Maridia/Outer.cs
+++ Randomizer.SMZ3/Regions/SuperMetroid/Maridia/Outer.cs
@@ -16,11 +16,12 @@ namespace Randomizer.SMZ3.Regions.SuperMetroid.Maridia {
                 }),
                 new Location(this, 137, 0x8FC43D, LocationType.Visible, "Super Missile (green Maridia)"),
                 new Location(this, 138, 0x8FC47D, LocationType.Visible, "Energy Tank, Mama turtle", Logic switch {
-                    Normal => items => items.CanFly() || items.SpeedBooster || items.Grapple,
-                    _ => new Requirement(items => items.CanFly() || items.SpeedBooster || items.Grapple ||
-                        items.CanSpringBallJump() && (items.Gravity || items.HiJump))
+                    Normal => items => items.CanOpenRedDoors() && (items.CanFly() || items.SpeedBooster || items.Grapple),
+                    _ => new Requirement(items => items.CanOpenRedDoors() && (items.CanFly() || items.SpeedBooster || items.Grapple ||
+                        items.CanSpringBallJump() && (items.Gravity || items.HiJump)))
                 }),
                 new Location(this, 139, 0x8FC483, LocationType.Hidden, "Missile (green Maridia tatori)"),
+                _ => new Requirement(items => items.CanOpenRedDoors())
             };
         }
 
@@ -28,12 +29,12 @@ namespace Randomizer.SMZ3.Regions.SuperMetroid.Maridia {
             return Logic switch {
                 Normal => items.Gravity && (
                         World.CanEnter("Norfair Upper West", items) && items.CanUsePowerBombs() ||
-                        items.CanAccessMaridiaPortal(World)
+                        items.CanAccessMaridiaPortal(World) && (items.CanPassBombPassages() || items.ScrewAttack)
                     ),
                 _ =>
                     World.CanEnter("Norfair Upper West", items) && items.CanUsePowerBombs() &&
                         (items.Gravity || items.HiJump && (items.CanSpringBallJump() || items.Ice)) ||
-                    items.CanAccessMaridiaPortal(World)
+                    items.CanAccessMaridiaPortal(World) && (items.CanPassBombPassages() || (items.Gravity && items.ScrewAttack) || items.Super && (items.Gravity || items.HiJump && (items.CanSpringBallJump() || items.Ice)
             };
         }
 
