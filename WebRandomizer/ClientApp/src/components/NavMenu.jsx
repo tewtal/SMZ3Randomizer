e36bcc9239d81ec988c2d255ce1a42a09662ff9d
Rebelus Quo <rebelusquo@tutanota.com>
2020-04-24 21:20:53 +0200

    Refactor the `NavMenu` component
    - The `Dropdown` component is already a `li` tag by virtue of the `nav` prop, so we drop the `NavItem` wrap.
    - Styled the `Navbar` and `NavbarBrand` specifically instead of using global style.
    - Used the reactstrap `Nav` component instead of an explicit `ul` tag.
    - Extracted common props between each `NavLink`.

--- WebRandomizer/ClientApp/src/components/NavMenu.jsx
+++ WebRandomizer/ClientApp/src/components/NavMenu.jsx
@@ -1,55 +1,52 @@
 import React, { useState } from 'react';
-import { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { Container, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
 import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import { Container, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, NavLink } from 'reactstrap';
 
-const NavMenuStyling = createGlobalStyle`
-  a.navbar-brand {
-    white-space: normal;
-    text-align: center;
-    word-break: break-all;
-  }
+const StyledNavbar = styled(Navbar)`
+  box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05);
+`;
 
-  .box-shadow {
-    box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05);
-  }
+const StyledNavbarBrand = styled(NavbarBrand)`
+  white-space: normal;
+  text-align: center;
+  word-break: break-all;
 `;
 
 export default function NavMenu(props) {
-    const [collapsed, setCollapsed] = useState(true);
     const { gameId } = props;
 
+    const [showMenu, setShowMenu] = useState(false);
+
+    const linkProps = { tag: Link, className: 'text-dark' };
     return (
-        <>
-            <NavMenuStyling />
-            <header>
-                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-                    <Container>
-                        <NavbarBrand tag={Link} to="/">Home</NavbarBrand>
-                        <NavbarToggler className="mr-2" onClick={() => setCollapsed(!collapsed)} />
-                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" navbar isOpen={!collapsed}>
-                            <ul className="navbar-nav flex-grow">
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to={`/configure/${gameId}`}>Generate randomized game</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <UncontrolledDropdown nav inNavbar>
-                                        <DropdownToggle className="text-dark" nav caret>Help</DropdownToggle>
-                                        <DropdownMenu>
-                                            <DropdownItem><NavLink tag={Link} className="text-dark" to="/information">Information</NavLink></DropdownItem>
-                                            <DropdownItem><NavLink tag={Link} className="text-dark" to="/mwinstructions">Multiworld instructions</NavLink></DropdownItem>
-                                            {gameId === "smz3" && <DropdownItem><NavLink tag={Link} className="text-dark" to="/logic">Logic Log</NavLink></DropdownItem>}
-                                            <DropdownItem><NavLink tag={Link} className="text-dark" to="/resources">Resources</NavLink></DropdownItem>
-                                            <DropdownItem><NavLink tag={Link} className="text-dark" to="/changelog">Changes</NavLink></DropdownItem>
-                                        </DropdownMenu>
-                                    </UncontrolledDropdown>
-                                </NavItem>
-                            </ul>
-                        </Collapse>
-                    </Container>
-                </Navbar>
-            </header>
-        </>
+        <header>
+            <StyledNavbar className="border-bottom mb-3" expand="sm" color="white" light>
+                <Container>
+                    <StyledNavbarBrand tag={Link} to="/">Home</StyledNavbarBrand>
+                    <NavbarToggler className="mr-2" onClick={() => setShowMenu(!showMenu)} />
+                    <Collapse className="d-sm-inline-flex flex-sm-row-reverse" navbar isOpen={showMenu}>
+                        <Nav className="flex-grow" navbar>
+                            <NavItem>
+                                <NavLink {...linkProps} to={`/configure/${gameId}`}>Generate randomized game</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle className="text-dark" nav caret>Help</DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem><NavLink {...linkProps} to="/information">Information</NavLink></DropdownItem>
+                                    <DropdownItem><NavLink {...linkProps} to="/mwinstructions">Multiworld instructions</NavLink></DropdownItem>
+                                    {gameId === "smz3" && (
+                                        <DropdownItem><NavLink {...linkProps} to="/logic">Logic Log</NavLink></DropdownItem>
+                                    )}
+                                    <DropdownItem><NavLink {...linkProps} to="/resources">Resources</NavLink></DropdownItem>
+                                    <DropdownItem><NavLink {...linkProps} to="/changelog">Changes</NavLink></DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </StyledNavbar>
+        </header>
     );
 }
