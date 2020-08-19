643e21ec4ed5d1ae78a77218b839b407092ad16f
Thomas Backmark <total@viskos.org>
2020-08-19 19:28:28 +0200

    Include Location data when applicable in the seed API

--- WebRandomizer/Controllers/SeedController.cs
+++ WebRandomizer/Controllers/SeedController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -24,12 +25,26 @@ namespace WebRandomizer.Controllers {
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetSeed(string seedGuid) {
             try {
-                var seedData = await context.Seeds.Include(x => x.Worlds).SingleOrDefaultAsync(x => x.Guid == seedGuid);
+                var seedData = await context.Seeds.Include(x => x.Worlds).ThenInclude(x => x.Locations).SingleOrDefaultAsync(x => x.Guid == seedGuid);
+
+                /* Clear out Location data if the ROM is a race ROM or the settings dict isn't available */
+                foreach (var world in seedData.Worlds) {
+                    try {
+                        var settings = JsonSerializer.Deserialize<Dictionary<string, string>>(world.Settings);
+                        if(settings["race"] == "true") {
+                            world.Locations = null;
+                        }
+                    } catch {
+                        world.Locations = null;
+                    }
+                }
+
                 if(seedData != null) {
                     return new OkObjectResult(seedData);
                 } else {
                     return new StatusCodeResult(404);
                 }
+            
             } catch {
                 return new StatusCodeResult(500);
             }
