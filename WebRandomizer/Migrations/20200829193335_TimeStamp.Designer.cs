100b0c90f5be9d530d4ed7828aa8157ab1245f4f
Thomas Backmark <total@viskos.org>
2020-08-27 21:42:50 +0200

    Make keysanity a configuration option for seeds.

--- WebRandomizer/Migrations/20200829193335_TimeStamp.Designer.cs
+++ WebRandomizer/Migrations/20200829193335_TimeStamp.Designer.cs
@@ -2,16 +2,18 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using Randomizer.Shared.Models;
+using WebRandomizer.Models;
 
 namespace WebRandomizer.Migrations
 {
     [DbContext(typeof(RandomizerContext))]
-    partial class RandomizerContextModelSnapshot : ModelSnapshot
+    [Migration("20200829193335_TimeStamp")]
+    partial class TimeStamp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
