b44e855ea90ff394bc49fe2bdd3d9f30de535148
Thomas Backmark <total@viskos.org>
2020-09-17 20:36:20 +0200

    Fixed issue with donation link

--- WebRandomizer/ClientApp/src/components/SmHome.jsx
+++ WebRandomizer/ClientApp/src/components/SmHome.jsx
@@ -41,7 +41,7 @@ export default function SmHome() {
                                 <input type="hidden" name="cmd" value="_s-xclick" />
                                 <input type="hidden" name="hosted_button_id" value="TD6E7WSKHXFA2" />
                                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-                                <img alt="" border="0" src="https://www.paypal.com/en_SE/i/scr/pixel.gif" width="1" height="1" />
+                                <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
                             </form>
                         </span>
                     </Col>
