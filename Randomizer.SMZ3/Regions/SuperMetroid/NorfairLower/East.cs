95098281de2ea888aa1a4a305ce5315d51e6f3ca
TarThoron <62519520+TarThoron@users.noreply.github.com>
2021-01-29 01:10:31 -0600

    Simplify logic and fix exit bug
    Moved Mickey Mouse to West to simplify logic, and fixed bug with CanExit that forced a gate glitch or reverse Amphitheater in Normal.

--- Randomizer.SMZ3/Regions/SuperMetroid/NorfairLower/East.cs
+++ Randomizer.SMZ3/Regions/SuperMetroid/NorfairLower/East.cs
@@ -12,55 +12,56 @@ namespace Randomizer.SMZ3.Regions.SuperMetroid.NorfairLower {
 
         public East(World world, Config config) : base(world, config) {
             Locations = new List<Location> {
-                new Location(this, 73, 0x8F8F30, LocationType.Visible, "Missile (Mickey Mouse room)", Logic switch {
-                    _ => new Requirement(items => CanExit(items) && items.Morph && items.CanDestroyBombWalls())
-                }),
                 new Location(this, 74, 0x8F8FCA, LocationType.Visible, "Missile (lower Norfair above fire flea room)", Logic switch {
-                    _ => new Requirement(items => CanExit(items) && items.CardLowerNorfairL1)
+                    _ => new Requirement(items => CanExit(items))
                 }),
                 new Location(this, 75, 0x8F8FD2, LocationType.Visible, "Power Bomb (lower Norfair above fire flea room)", Logic switch {
-                    Normal => new Requirement(items => CanExit(items) && items.CardLowerNorfairL1),
-                    _ => items => CanExit(items) && items.CardLowerNorfairL1 && items.CanPassBombPassages()
+                    Normal => new Requirement(items => CanExit(items)),
+                    _ => items => CanExit(items) && items.CanPassBombPassages()
                 }),
                 new Location(this, 76, 0x8F90C0, LocationType.Visible, "Power Bomb (Power Bombs of shame)", Logic switch {
-                    _ => new Requirement(items => CanExit(items) && items.CardLowerNorfairL1 && items.CanUsePowerBombs())
+                    _ => new Requirement(items => CanExit(items) && items.CanUsePowerBombs())
                 }),
                 new Location(this, 77, 0x8F9100, LocationType.Visible, "Missile (lower Norfair near Wave Beam)", Logic switch {
-                    Normal => new Requirement(items => CanExit(items) && items.CardLowerNorfairL1),
-                    _ => items => CanExit(items) && items.CardLowerNorfairL1 && items.Morph && items.CanDestroyBombWalls()
+                    Normal => new Requirement(items => CanExit(items)),
+                    _ => items => CanExit(items) && items.Morph && items.CanDestroyBombWalls()
                 }),
                 new Location(this, 78, 0x8F9108, LocationType.Hidden, "Energy Tank, Ridley", Logic switch {
-                    _ => new Requirement(items => CanExit(items) && items.CardLowerNorfairL1 && items.CardLowerNorfairBoss && items.CanUsePowerBombs() && items.Super)
+                    _ => new Requirement(items => CanExit(items) && items.CardLowerNorfairBoss && items.CanUsePowerBombs() && items.Super)
                 }),
                 new Location(this, 80, 0x8F9184, LocationType.Visible, "Energy Tank, Firefleas", Logic switch {
-                    _ => new Requirement(items => CanExit(items) && items.CardLowerNorfairL1)
+                    _ => new Requirement(items => CanExit(items))
                 })
             };
         }
 
-        private bool CanExit(Progression items) {
-            return Logic switch
-            {
-                Normal => !Config.Keysanity || (items.CardNorfairL2 || items.Gravity && (items.Wave || items.SpaceJump && items.CanUsePowerBombs())),
-                _ => true
+        bool CanExit(Progression items) {
+            return Logic switch {
+                Normal => items.CardNorfairL2 /*Bubble Mountain*/ ||
+                    items.Gravity && items.Wave /* Volcano Room and Blue Gate */ && (items.Grapple || items.SpaceJump /*Spikey Acid Snakes and Croc Escape*/),
+                _ => /*Vanilla LN Escape*/
+                    (items.Morph && (items.CardNorfairL2 /*Bubble Mountain*/ || (items.Missile || items.Super || items.Wave /* Blue Gate */) && 
+                                     (items.SpeedBooster || items.CanFly() || items.Grapple || items.HiJump && 
+                                     (items.CanSpringBallJump() || items.Ice) /*Frog Speedway or Croc Escape*/)) ||
+                    /*Reverse Amphitheater*/
+                     items.HasEnergyReserves(5),
             };
         }
 
         public override bool CanEnter(Progression items) {
             return Logic switch {
                 Normal =>
-                    items.Varia && (
+                    items.Varia && items.CardLowerNorfairL1 && (
                         World.CanEnter("Norfair Upper East", items) && items.CanUsePowerBombs() && items.SpaceJump && items.Gravity ||
                         items.CanAccessNorfairLowerPortal() && items.CanDestroyBombWalls() && items.Super && items.CanUsePowerBombs() && items.CanFly()
                     ),
                 _ =>
-                    items.Varia && (
+                    items.Varia && items.CardLowerNorfairL1 && (
                         World.CanEnter("Norfair Upper East", items) && items.CanUsePowerBombs() && (items.HiJump || items.Gravity) ||
                         items.CanAccessNorfairLowerPortal() && items.CanDestroyBombWalls() && items.Super && (items.CanFly() || items.CanSpringBallJump() || items.SpeedBooster)
                     ) &&
                     (items.CanFly() || items.HiJump || items.CanSpringBallJump() || items.Ice && items.Charge) &&
-                    (items.CanPassBombPassages() || items.ScrewAttack && items.SpaceJump) &&
-                    (items.Morph || items.HasEnergyReserves(5))
+                    (items.CanPassBombPassages() || items.ScrewAttack && items.SpaceJump)                     
             };
         }
 
