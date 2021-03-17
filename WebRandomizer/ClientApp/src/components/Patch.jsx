d26283f7a4df797d83dc6ba281dacf3498214879
Thomas Backmark <total@viskos.org>
2021-03-17 15:02:47 +0100

    Fix download of older seeds with missing options.

--- WebRandomizer/ClientApp/src/components/Patch.jsx
+++ WebRandomizer/ClientApp/src/components/Patch.jsx
@@ -134,9 +134,9 @@ export default function Patch(props) {
         function smz3Parts({ smlogic, swordlocation, morphlocation, keyshuffle }) {
             return [
                 `ZLn+SL${smlogic[0]}`,
-                swordlocation !== 'randomized' ? `S${swordlocation[0]}` : null,
-                morphlocation !== 'randomized' ? `M${morphlocation[0]}` : null,
-                keyshuffle !== 'none' ? `K${keyshuffle[0]}` : null
+                swordlocation && swordlocation !== 'randomized' ? `S${swordlocation[0]}` : null,
+                morphlocation && morphlocation !== 'randomized' ? `M${morphlocation[0]}` : null,
+                keyshuffle && keyshuffle !== 'none' ? `K${keyshuffle[0]}` : null
             ];
         }
 
