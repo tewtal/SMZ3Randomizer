4622d5a15940791549cc4febbcdf251b88460b17
Rebelus Quo <rebelusquo@tutanota.com>
2021-09-25 18:40:41 +0200

    Adjust code style

--- WebRandomizer/Controllers/Helpers.cs
+++ WebRandomizer/Controllers/Helpers.cs
@@ -1,12 +1,10 @@
-﻿using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebRandomizer.Controllers {
+
     public class Helpers {
         public static string SerializeEnumAsString(object ob) {
             return JsonConvert.SerializeObject(
@@ -21,14 +19,15 @@ namespace WebRandomizer.Controllers {
 
         public static string GetSeedHashNames(Dictionary<int, byte[]> patch, string gameId) {
             string[] names = {
-                "GEEMER", "RIPPER", "ATOMIC", "POWAMP", "SCISER", "NAMIHE", "PUROMI", "ALCOON",
-                "BEETOM","OWTCH","ZEBBO","ZEELA","HOLTZ","VIOLA","WAVER","RINKA",
-                "BOYON","CHOOT","KAGO","SKREE","COVERN","EVIR","TATORI","OUM",
-                "PUYO","YARD","ZOA","FUNE","GAMET","GERUTA","SOVA","BULL",
-                "ARRGI","BABUSU","BORU","HACHI","BABURU","TAINON","GERUDO","GIBO",
-                "KOPPI","PON","HOBA","HYU","KISU","KYUNE","RIBA","MEDUSA",
-                "TERU","FANGIN","PIKKU","POPO","NOMOSU","GUZU","AIGORU","ROPA",
-                "GAPURA","HEISHI","SUTARU","TOZOKU","TOPPO","WAINDA","KURIPI","ZORA"};
+                "GEEMER","RIPPER","ATOMIC","POWAMP","SCISER","NAMIHE","PUROMI","ALCOON",
+                "BEETOM","OWTCH", "ZEBBO", "ZEELA", "HOLTZ", "VIOLA", "WAVER", "RINKA",
+                "BOYON", "CHOOT", "KAGO",  "SKREE", "COVERN","EVIR",  "TATORI","OUM",
+                "PUYO",  "YARD",  "ZOA",   "FUNE",  "GAMET", "GERUTA","SOVA",  "BULL",
+                "ARRGI", "BABUSU","BORU",  "HACHI", "BABURU","TAINON","GERUDO","GIBO",
+                "KOPPI", "PON",   "HOBA",  "HYU",   "KISU",  "KYUNE", "RIBA",  "MEDUSA",
+                "TERU",  "FANGIN","PIKKU", "POPO",  "NOMOSU","GUZU",  "AIGORU","ROPA",
+                "GAPURA","HEISHI","SUTARU","TOZOKU","TOPPO", "WAINDA","KURIPI","ZORA",
+            };
 
             if (gameId == "smz3") {
                 var hashData = patch[0x420000];
@@ -38,5 +37,7 @@ namespace WebRandomizer.Controllers {
                 return $"{names[hashData[0] & 0x1F]} {names[hashData[1] & 0x1F]} {names[hashData[2] & 0x1F]} {names[hashData[3] & 0x1F]}";
             }
         }
+
     }
+
 }
