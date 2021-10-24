1f97e56787f101834766c90ef84f7b5cb640d305
RebelusQuo <rebelusquo@tutanota.com>
2021-10-24 23:34:04 +0200

    Forward the cancellation token to the context

--- WebRandomizer/Controllers/RandomizerController.cs
+++ WebRandomizer/Controllers/RandomizerController.cs
@@ -98,7 +98,7 @@ namespace WebRandomizer.Controllers {
                 }
 
                 context.Add(seed);
-                await context.SaveChangesAsync();
+                await context.SaveChangesAsync(cancellationToken);
 
                 /* If this is a multiworld seed, we also create a new multiworld session with the same session guid as the seed guid */
                 if (seed.Players > 1) {
@@ -110,7 +110,7 @@ namespace WebRandomizer.Controllers {
                     };
 
                     context.Add(session);
-                    await context.SaveChangesAsync();
+                    await context.SaveChangesAsync(cancellationToken);
                 }
 
                 return new OkObjectResult(seed);
