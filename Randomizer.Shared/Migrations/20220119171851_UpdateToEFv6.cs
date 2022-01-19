5130683d13d24d18be6bc1e19c1df235a8fc1800
RebelusQuo <rebelusquo@tutanota.com>
2022-01-19 18:21:14 +0100

    Update Entity Framework to v6

---
+++ Randomizer.Shared/Migrations/20220119171851_UpdateToEFv6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebRandomizer.Migrations
+{
+    public partial class UpdateToEFv6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeStamp",
+                table: "Events",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TimeStamp",
+                table: "Events",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
