e1bb38de35dc054d301e5d36dad6bbd9e9180886
RebelusQuo <rebelusquo@tutanota.com>
2022-03-03 22:24:45 +0100

    Place SM rewards with bias against pendants
    The rewards for SM are generated from a kind of distribution table which in
    effect yield the following probabilities for the different number of pendants:
    0: 66.667%
    1: 30.224%
    2:  2.994%
    3:  0.062%
    
    The rewards (and medallion requirements) are still stored as a sequence. I
    argue that it's better to switch to storing the data as a mapping, against some
    abbreviations as keys, on the next major version jump.

---
+++ Randomizer.SMZ3/util/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Randomizer.SMZ3 {
+
+    static class Extensions {
+
+        // The opposite of Aggregate, instead of combining elements into a
+        // value, `Generate` provides elements from a seed argument.
+        public static IEnumerable<TResult> Generate<TSeed, TResult>(
+            this TSeed seed, Func<TSeed, (TResult, TSeed)> func
+        ) {
+            TResult result;
+            while (true) {
+                (result, seed) = func(seed);
+                yield return result;
+            }
+        }
+
+    }
+
+}
